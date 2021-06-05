c9cd4906752d3a723fdd1bd841b74547404670ca
thaisc98 <thaisc98@hotmail.es>
2021-06-05 19:50:50 -0400

    [fix] try to build th wrap

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -1,7 +1,24 @@
 (ns kata.core
-  (:gen-class))
+  (:gen-class)
+  (:use [clojure.string :only (last-index-of)]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn letter-is-grater [letter col]
+  (<= (count letter) col))
+
+(defn substring [letter col]
+  (subs letter 0 col))
+
+(defn last-index [letter]
+  (last-index-of letter " "))
+
+(defn get-space [letter col]
+  (last-index (subs letter 0  col)))
+
+
+(defn wrapper [letter col]
+  (if (letter-is-grater letter col) (str letter) (str col)))
--- test/kata/core_test.clj
+++ test/kata/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [kata.core :refer :all]))
 
+;; morse -> text
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "FIXED"
+    (is (= 1 1))))
+
+(deftest letter-is-grater-test
+  (testing "letter-is-grater-test"
+    (is (= (letter-is-grater "...." 3) false))))
+
+(deftest wrapper-test
+  (testing "letter-is-grater-test"
+    (is (= (wrapper "...." 3) "3"))))
+
+(deftest substring-test
+  (testing "substring-test"
+    (is (= (substring "...." 3) "..."))))
+
+(deftest last-index-test
+  (testing "last-index-test"
+    (is (= (last-index "... ") 3))))
+
+(deftest last-index-test
+  (testing "last-index-test"
+    (is (= (get-space ".... ---" 3) 3))))
+
+;; .... --- .-.. .-    => Ho
